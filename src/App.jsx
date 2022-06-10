9d7d3bbb8940fe7dd8a6890d833a012ce0d048e1
Lopez Delgado Jose Andres <isc17350296@gmail.com>
2022-06-10 09:52:29 -0500

    segundo commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,63 +1,84 @@
 import {useState} from "react";
-
-function App(){
+function App (){
 
   const [cuenta, setCuenta] = useState(0);
   const [paso, setPaso] = useState(1);
 
-const handleMinusClick = () => {
-  console.log(paso);
-setCuenta(cuenta - paso);
-};
-const handleResetClick = () => {
-  setCuenta(0);
-  setPaso(1);
-};
-const handlePlusClick = () => {
-  setCuenta(cuenta + paso);
-};
+  const handleMinusClick = () => {
+    setCuenta(cuenta - paso);
+  };
 
+  const handleResetClick = () => {
+    setCuenta(0);
+    setPaso(1);
+  };
 
-const handleInputChange = (e) => {
-   if(isNaN(e.target.value)){
-     return;
-   }
-   setPaso(Number(e.target.value));
-};
-SetCuenta
-};
+  const handlePlusClick = () => {
+    setCuenta(cuenta + paso);
+  };
 
-    return(
-        <div className="App">
-        <h3>Contador</h3>
-        <hr />
-        <h2 className="text-center">0</h2>
-        <hr />
-        <div style={{
-          display: "flex", 
-          justifyContent: "flex-end",
-          marginRight: "5px",
-        }}
-        >
-          <label>
-            Paso
-            <input id="" name="" type="text" onChange={handleInputChange} style={{marginLeft: "5px", width: "60px" }
-            }}/>
-          </label>
-          <button type="button" className="btn 
-          btn-primary" onClick={() => {
-            onclick={handleMinusClick}
-            style={{marginleft: "5px"}}
+  const handleInputChange = (e) => {
+
+    if (isNaN(e.target.value)){
+      return;
+    }
+    setPaso(Number(e.target.value));
+  };
+
+  return (
+
+    <div className="App">
+      <h3> Contador </h3>
+      <hr /> 
+      <h2 className="text-center"> {cuenta} </h2>
+      <hr />
+
+      <div style= {{ display: "flex", 
+        justifyContent:"flex-end",
+        marginRight: "5px",
+        }}> 
+
+        <label>
+          Paso
+          <input 
+          id="paso"
+          type= "text" 
+          onChange = {handleInputChange}
+          value={paso}
+          style= {{marginLeft: "5px", width: "60 px"}}
+          />
+        </label>
+        
+        <button 
+          type = "button" 
+          className ="btn btn-primary"
+          onClick={handleMinusClick}
+          style= {{ marginLeft: "5px"}}
           >
-          <button type="button" className="btn 
-          btn-primary" onClick={() => {
-            onclick={handleResetClick}
-            style={{marginleft: "5px"}}
-        -1
-        </button>
-        </div>
-        </div>
-   );
+            -1
+            
+          </button>
+
+        <button 
+          type = "button" 
+          className ="btn btn-primary"
+          onClick={handleResetClick}
+          style= {{marginLeft: "5px"}}
+          >
+            RESET
+            
+          </button>
+
+        <button 
+          type = "button" 
+          className ="btn btn-primary"
+          onClick={handlePlusClick}
+          style= {{marginLeft: "5px"}}
+          >+1</button>
+      </div>
+    </div>
+  
+);
 }
 
-export default App
+export default App;
